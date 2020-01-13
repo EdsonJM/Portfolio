f50c4fe73ba1a29f02db1289fd89d9753d40d6fa
Edson <edsonjm99@gmail.com>
2020-01-12 21:58:03 -0600

    Added Project Modal Description

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,3 +31,30 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+//Functionality for Work Page
+
+// Get modal element
+let modal = document.getElementById("simpleModal");
+
+// Get open modal button
+let modalBtn = document.getElementById("modalBtn");
+
+// Get close button
+let closeBtn = document.getElementsByClassName("closeBtn")[0];
+
+// Listen for open click
+modalBtn.addEventListener("click", openModal);
+
+// Listen for outside click
+window.addEventListener("click", clickOutSide);
+
+function openModal() {
+  modal.style.display = "block";
+}
+
+function clickOutSide(e) {
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
+}
